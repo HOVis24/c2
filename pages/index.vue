5b44461533dc8b921ea4132073ac364ac9d48796
Jan Dean <98754979+IamDean10@users.noreply.github.com>
2022-09-20 10:43:45 +0800

    feat: revert to nuxt 2

--- pages/index.vue
+++ pages/index.vue
@@ -1,60 +1,16 @@
 <template>
+ <p>Test</p>
+</template>
 
-
-
-<div class="main-container">
-      <div class="image-container"></div>
-      <div id="pumpkin">A pumpkin halloween decoration? In September? Weird!</div>
-      
-    </div>
-    <Script src="js/three.min.js"></Script>
-    <Script src="js/panolens.min.js"></Script>
-    <Script src="js/main.js"></Script>
-    
-  </template>
-  
-  <script>
-import Panolens from 'panolens'; 
-
-let TWEEN    = Panolens.TWEEN;
-let PANOLENS = Panolens.PANOLENS;
-
-panorama = new PANOLENS.ImagePanorama('images/panoramic.png');
-  </script>
-
-  <style>
-    body {
-  margin: 0;
+<script>
+export default {
+  name: 'IndexPage',
+  mounted(){
+    const PANOLENS = window.PANOLENS;
+    const panorama = new PANOLENS.ImagePanorama( '/field.jpg' );
+    const viewer = new PANOLENS.Viewer();
+    viewer.add( panorama );
+  }
 }
+</script>
 
-.main-container {
-  display: flex;
-  height: 100vh;
-  align-items: center;
-  background: #111;
-}
-
-.main-container .image-container {
-  /* flex: 1; */
-  height: 100%;
-
-  position: fixed;
-  width: 100%;
-}
-
-.main-container h1 {
-  flex: 1;
-  text-align: center;
-  color: #fff;
-  font-family: "Raleway", sans-serif;
-  text-transform: uppercase;
-  font-size: 60px;
-  font-weight: 800;
-  letter-spacing: 10px;
-
-  z-index: 100;
-  text-shadow: 2px 16px 16px rgba(0, 0, 0, 0.3);
-  pointer-events: none;
-}
-
-  </style>
